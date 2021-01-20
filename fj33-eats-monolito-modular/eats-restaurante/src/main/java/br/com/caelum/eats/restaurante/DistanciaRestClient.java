00c0068e8bdf95ad469e0de21017fa5c0fd8c194
Alefh Sousa <alefh.sousa@gmail.com>
2021-01-19 21:25:11 -0300

    adiciona spring retry

--- fj33-eats-monolito-modular/eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/DistanciaRestClient.java
+++ fj33-eats-monolito-modular/eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/DistanciaRestClient.java
@@ -3,6 +3,8 @@ package br.com.caelum.eats.restaurante;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,11 +21,16 @@ class DistanciaRestClient {
     this.restTemplate = restTemplate;
   }
 
+
+  //jitter
+  @Retryable(maxAttempts = 5, backoff = @Backoff(delay = 1000, multiplier = 3, random = true ))
   void novoRestauranteAprovado(Restaurante restaurante) {
     RestauranteParaServicoDeDistancia restauranteParaDistancia = new RestauranteParaServicoDeDistancia(restaurante);
     String url = distanciaServiceUrl+"/restaurantes";
-    ResponseEntity<RestauranteParaServicoDeDistancia> responseEntity =
-        restTemplate.postForEntity(url, restauranteParaDistancia, RestauranteParaServicoDeDistancia.class);
+    ResponseEntity<RestauranteParaServicoDeDistancia> responseEntity = restTemplate.postForEntity(url, restauranteParaDistancia, RestauranteParaServicoDeDistancia.class);
+
+    ResponseEntity<RestauranteParaServicoDeDistancia> responseEntity2 = restTemplate.
+            postForEntity(url, restauranteParaDistancia, RestauranteParaServicoDeDistancia.class);
     HttpStatus statusCode = responseEntity.getStatusCode();
     if (!HttpStatus.CREATED.equals(statusCode)) {
       throw new RuntimeException("Status diferente do esperado: " + statusCode);
